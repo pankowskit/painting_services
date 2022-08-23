19ffed4b6da8b5f12de2e57219c481d35bd7de99
pankowskit <dagi@10g.pl>
2022-08-23 08:21:59 +0200

    layout

--- src/components/about-s1.js
+++ src/components/about-s1.js
@@ -2,34 +2,34 @@ import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
 import Row from "../../node_modules/react-bootstrap/Row";
 import Col from "../../node_modules/react-bootstrap/Col";
-import Button from "../../node_modules/react-bootstrap/Button";
 
 class AboutS1 extends Component{
     render(){
         return(    
-            <Container fluid className="about-s1 d-flex bg-dark minh-50vh align-items-center py-5">
-            <Row className="mx-0 w-100 mt-5">
-                <Col xs={{span:12,order:1}} md={{span:7,order:2}} className="minh-25vh"></Col>
-                <Col xs={{span:12,order:2}} md={{span:5,order:1}} className="minh-50vh d-flex align-items-center">
-                    <div className="w-100 text-center text-md-start">
-                        <h5 className="text-white">
-                            Sed ut perspiciatis
-                        </h5>
-                        <h2 className="dispaly-6 fw-bold text-white-50">
-                            Unde omnis iste
-                        </h2>
-                        <p className="text-white">
-                            Natus error sit voluptatem accusantium doloremque laudantium, 
-                            totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
-                            et quasi architecto beatae vitae dicta sunt explicabo. Nemo 
-                            enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut 
-                            fugit, sed quia consequuntur magni dolores eos qui ratione 
-                            voluptatem sequi nesciunt. Neque porro quisquam est
-                        </p>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+            <Container fluid className="d-flex bg-secondary minh-50vh align-items-center py-5 border-top ">
+                <Row className="mx-0 w-100">
+                    <Col xs={12} md={7} className="minh-25vh d-none d-md-block"></Col>
+                    <Col xs={12} md={5} className="minh-50vh d-flex align-items-center">
+                        <div className="w-100 text-center text-md-start pt-5 text-shadow">
+                            <h5 className="text-white">
+                                Lorem ipsum
+                            </h5>
+                            <h2 className="dispaly-6 fw-bold text-white-50 text-shadow">
+                                Dolor sit amet         
+                            </h2>
+                            <p className="text-white">
+                                Sed do eiusmod tempor incididunt ut labore et dolore magna 
+                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                Duis aute irure dolor in reprehenderit in voluptate velit 
+                                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                                occaecat cupidatat non proident, sunt in culpa qui officia 
+                                deserunt mollit anim id est laborum.      
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
